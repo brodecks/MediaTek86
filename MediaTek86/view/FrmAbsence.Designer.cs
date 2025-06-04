6703668e5375144fc70f41132a0ea80f13b3cbbb
brodeck <raphael.donikian@outlook.fr>
2025-06-04 21:59:05 +0200

    Etape 2 : dessiner les interfaces, structurer l'appli en MVC, coder le visuel

---
+++ MediaTek86/view/FrmAbsence.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MediaTek86
+{
+    partial class FrmAbsence
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAbsence = new System.Windows.Forms.DataGridView();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAbsence)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAbsence
+            // 
+            this.dgvAbsence.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAbsence.Location = new System.Drawing.Point(0, 0);
+            this.dgvAbsence.Name = "dgvAbsence";
+            this.dgvAbsence.RowHeadersWidth = 51;
+            this.dgvAbsence.RowTemplate.Height = 24;
+            this.dgvAbsence.Size = new System.Drawing.Size(424, 229);
+            this.dgvAbsence.TabIndex = 0;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(117, 235);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSupprimer.TabIndex = 1;
+            this.btnSupprimer.Text = "supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(220, 235);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifier.TabIndex = 2;
+            this.btnModifier.Text = "modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(12, 235);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 3;
+            this.btnAjouter.Text = "ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(334, 235);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.btnAnnuler.TabIndex = 4;
+            this.btnAnnuler.Text = "annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // FrmAbsence
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 271);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.dgvAbsence);
+            this.Name = "FrmAbsence";
+            this.Text = "FrmAbsence";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAbsence)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAbsence;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnAnnuler;
+    }
+}
--- MediaTek86/view/FrmMediaTek86.Designer.cs
+++ MediaTek86/view/FrmMediaTek86.Designer.cs
@@ -28,20 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvPersonnel = new System.Windows.Forms.DataGridView();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnAbsences = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.lstServiceAffect = new System.Windows.Forms.ListBox();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonnel)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvPersonnel
+            // 
+            this.dgvPersonnel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersonnel.Location = new System.Drawing.Point(0, 0);
+            this.dgvPersonnel.Name = "dgvPersonnel";
+            this.dgvPersonnel.RowHeadersWidth = 51;
+            this.dgvPersonnel.RowTemplate.Height = 24;
+            this.dgvPersonnel.Size = new System.Drawing.Size(713, 236);
+            this.dgvPersonnel.TabIndex = 0;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(-1, 251);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 1;
+            this.btnAjouter.Text = "ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(80, 251);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSupprimer.TabIndex = 2;
+            this.btnSupprimer.Text = "supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(170, 251);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifier.TabIndex = 3;
+            this.btnModifier.Text = "modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // btnAbsences
+            // 
+            this.btnAbsences.Location = new System.Drawing.Point(260, 251);
+            this.btnAbsences.Name = "btnAbsences";
+            this.btnAbsences.Size = new System.Drawing.Size(75, 23);
+            this.btnAbsences.TabIndex = 4;
+            this.btnAbsences.Text = "absences";
+            this.btnAbsences.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "prénom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "tel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 357);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "mail";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(263, 351);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(164, 22);
+            this.txtMail.TabIndex = 9;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(55, 354);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(152, 22);
+            this.txtTel.TabIndex = 10;
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(263, 301);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(164, 22);
+            this.txtPrenom.TabIndex = 11;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(55, 301);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(152, 22);
+            this.txtNom.TabIndex = 12;
+            // 
+            // lstServiceAffect
+            // 
+            this.lstServiceAffect.FormattingEnabled = true;
+            this.lstServiceAffect.ItemHeight = 16;
+            this.lstServiceAffect.Location = new System.Drawing.Point(470, 301);
+            this.lstServiceAffect.Name = "lstServiceAffect";
+            this.lstServiceAffect.Size = new System.Drawing.Size(172, 84);
+            this.lstServiceAffect.TabIndex = 13;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(352, 251);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.btnAnnuler.TabIndex = 14;
+            this.btnAnnuler.Text = "annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
             // 
             // FrmMediatek
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(714, 536);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.lstServiceAffect);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAbsences);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.dgvPersonnel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmMediatek";
             this.Text = "MediaTek 86";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonnel)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvPersonnel;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnAbsences;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.ListBox lstServiceAffect;
+        private System.Windows.Forms.Button btnAnnuler;
     }
 }
 
