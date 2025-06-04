43c068b2f51dfd2ec048acaeaa07acef1d7453ad
Brodecks <raphae.donikian@oulootk.fr>
2025-06-04 17:54:13 +0200

    Ajoutez des fichiers projet.

---
+++ MediaTek86/FrmAuthentification.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MediaTek86
+{
+    partial class FrmAuthentification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnecter = new System.Windows.Forms.Button();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnecter
+            // 
+            this.btnConnecter.Location = new System.Drawing.Point(80, 78);
+            this.btnConnecter.Name = "btnConnecter";
+            this.btnConnecter.Size = new System.Drawing.Size(83, 23);
+            this.btnConnecter.TabIndex = 0;
+            this.btnConnecter.Text = "Se connecter";
+            this.btnConnecter.UseVisualStyleBackColor = true;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(80, 48);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(100, 20);
+            this.txtPwd.TabIndex = 1;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(80, 22);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 2;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(24, 22);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(29, 13);
+            this.lblLogin.TabIndex = 3;
+            this.lblLogin.Text = "login";
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Location = new System.Drawing.Point(24, 51);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(27, 13);
+            this.lblPwd.TabIndex = 4;
+            this.lblPwd.Text = "pwd";
+            // 
+            // FrmAuthentification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 115);
+            this.Controls.Add(this.lblPwd);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.btnConnecter);
+            this.Name = "FrmAuthentification";
+            this.Text = "Connexion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnecter;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblPwd;
+    }
+}
